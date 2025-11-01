996514618312e39d65d29e99b7384772bedcfd55
WG-SOL <32182825@dankook.ac.kr>
2025-11-01 21:50:36 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int tree[26][2];
+
+void PreOrder(int now) {
+    if (now == -1) return;
+
+    cout << (char)(now + 'A');
+    PreOrder(tree[now][0]);
+    PreOrder(tree[now][1]);
+
+}
+
+void InOrder(int now) {
+    if (now == -1) return;
+
+    InOrder(tree[now][0]);
+    cout << (char)(now + 'A');
+    InOrder(tree[now][1]);
+}
+
+void PostOrder(int now) {
+    if (now == -1) return;
+
+    PostOrder(tree[now][0]);
+    PostOrder(tree[now][1]);
+    cout << (char)(now + 'A');
+
+}
+
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        char node_char, left, right;
+        cin >> node_char >> left >> right;
+        int node = node_char - 'A';
+
+        if (left == '.') tree[node][0] = -1;
+        else tree[node][0] = left - 'A';
+
+        if (right == '.') tree[node][1] = -1;
+        else tree[node][1] = right - 'A';
+    }
+
+    PreOrder(0);
+    cout << "\n";
+    InOrder(0);
+    cout << "\n";
+    PostOrder(0);
+
+}
